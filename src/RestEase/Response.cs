f069f2e4b0d472079523d937c58ef16241c39e77
Antony Male <antony.male@gmail.com>
2015-04-24 17:34:50 +0100

    Tiny bit more documenteering

--- src/RestEase/Response.cs
+++ src/RestEase/Response.cs
@@ -7,9 +7,20 @@ using System.Threading.Tasks;
 
 namespace RestEase
 {
+    /// <summary>
+    /// Response containing both the HttpResponseMessage and deserialized response
+    /// </summary>
+    /// <typeparam name="T">Type of deserialized response</typeparam>
     public class Response<T>
     {
+        /// <summary>
+        /// Raw HttpResponseMessage
+        /// </summary>
         public HttpResponseMessage ResponseMessage { get; private set; }
+
+        /// <summary>
+        /// Deserialized response
+        /// </summary>
         public T Content { get; private set; }
 
         public Response(HttpResponseMessage response, T content)
