d9efd8e113552afd2c423c7ef0eccaa2cd7adbc3
Antony Male <antony.male@gmail.com>
2020-05-29 12:13:51 +0100

    Fix doc warnings

--- src/RestEase/Implementation/RestEaseInterfaceImplementationAttribute.cs
+++ src/RestEase/Implementation/RestEaseInterfaceImplementationAttribute.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel;
 
 namespace RestEase.Implementation
 {
+    /// <summary>
+    /// Internal type. Do not use.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
     [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
     public sealed class RestEaseInterfaceImplementationAttribute : Attribute
     {
+        /// <summary>
+        /// Internal type. Do not use.
+        /// </summary>
         public Type InterfaceType { get; }
+
+        /// <summary>
+        /// Internal type. Do not use.
+        /// </summary>
         public Type ImplementationType { get; }
 
+        /// <summary>
+        /// Internal type. Do not use.
+        /// </summary>
         public RestEaseInterfaceImplementationAttribute(Type interfaceType, Type implementationType)
         {
             this.InterfaceType = interfaceType;
