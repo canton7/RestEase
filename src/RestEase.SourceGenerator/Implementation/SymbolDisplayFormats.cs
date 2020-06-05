ea6a4b3b2323a1ea7b37ba73dc13c728f27406f6
Antony Male <antony.male@gmail.com>
2020-06-05 19:29:34 +0100

    WIP documenting SymbolDisplayFormats

--- src/RestEase.SourceGenerator/Implementation/SymbolDisplayFormats.cs
+++ src/RestEase.SourceGenerator/Implementation/SymbolDisplayFormats.cs
@@ -4,13 +4,44 @@ namespace RestEase.SourceGenerator.Implementation
 {
     internal static class SymbolDisplayFormats
     {
+        /// <summary>
+        /// The name of a symbol, suitably escaped. E.g. "List" or "MethodName"
+        /// </summary>
         public static SymbolDisplayFormat SymbolName { get; }
+
+        /// <summary>
+        /// The full name of a namespace, suitably escaped. E.g. "System.Collections.Generic"
+        /// </summary>
         public static SymbolDisplayFormat Namespace { get; }
+        /// <summary>
+        /// The name of a class, including type parameters, suitably escaped. E.g. "List&lt;T&gt;"
+        /// </summary>
         public static SymbolDisplayFormat ClassName { get; }
+        /// <summary>
+        /// The fully-qualified name name of a class, including type parameters and constraints, suitably escaped.
+        /// E.g. "global::My.Namespace.Foo&lt;T&gt; where T : global::System.Collections.Generic.IEnumerable&lt;T&gt;"
+        /// </summary>
         public static SymbolDisplayFormat QualifiedClassNameWithTypeConstraints { get; }
+        /// <summary>
+        /// A type name, suitable for appending to a prefix for use as a constructor. No escaping.
+        /// E.g. "IFoo".
+        /// </summary>
         public static SymbolDisplayFormat ConstructorName { get; }
+        /// <summary>
+        /// The fully-qualified name of an interface that's being implemented, including type parameters (but excluding
+        /// type constraints), suitably escaped. E.g. "global::My.Namespace.IFoo&lt;T&gt;"
+        /// </summary>
         public static SymbolDisplayFormat ImplementedInterface { get; }
+        /// <summary>
+        /// A string suitable for use when declaring a property, suitably escaped. Includes getter/setter but doesn't
+        /// include a return type or accessibility. E.g. "Foo { get; set; }"
+        /// </summary>
         public static SymbolDisplayFormat PropertyDeclaration { get; }
+        /// <summary>
+        /// A string suitable for use when declaring a method, suitably escaped. Doesn't include return type or accessibility,
+        /// but includes parameters, type constraints, default values. 
+        /// E.g. "Foo<T>
+        /// </summary>
         public static SymbolDisplayFormat ImplicitMethodDeclaration { get; }
         public static SymbolDisplayFormat ExplicitMethodDeclaration { get; }
         public static SymbolDisplayFormat MethodOrPropertyReturnType { get; }
@@ -39,7 +70,8 @@ namespace RestEase.SourceGenerator.Implementation
                 typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces,
                 genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters
                     | SymbolDisplayGenericsOptions.IncludeTypeConstraints,
-                miscellaneousOptions: SymbolDisplayMiscellaneousOptions.EscapeKeywordIdentifiers);
+                miscellaneousOptions: SymbolDisplayMiscellaneousOptions.EscapeKeywordIdentifiers
+                    | SymbolDisplayMiscellaneousOptions.UseSpecialTypes);
 
             ConstructorName = new SymbolDisplayFormat();
 
@@ -51,18 +83,13 @@ namespace RestEase.SourceGenerator.Implementation
                     | SymbolDisplayMiscellaneousOptions.UseSpecialTypes);
 
             PropertyDeclaration = new SymbolDisplayFormat(
-                globalNamespaceStyle: SymbolDisplayGlobalNamespaceStyle.Included,
-                typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces,
-                genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters,
                 propertyStyle: SymbolDisplayPropertyStyle.ShowReadWriteDescriptor,
-                miscellaneousOptions: SymbolDisplayMiscellaneousOptions.EscapeKeywordIdentifiers
-                    | SymbolDisplayMiscellaneousOptions.UseSpecialTypes);
+                miscellaneousOptions: SymbolDisplayMiscellaneousOptions.EscapeKeywordIdentifiers);
 
             ImplicitMethodDeclaration = new SymbolDisplayFormat(
                 globalNamespaceStyle: SymbolDisplayGlobalNamespaceStyle.Included,
                 typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces,
                 genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters
-                    | SymbolDisplayGenericsOptions.IncludeVariance
                     | SymbolDisplayGenericsOptions.IncludeTypeConstraints,
                 memberOptions: SymbolDisplayMemberOptions.IncludeParameters
                     | SymbolDisplayMemberOptions.IncludeConstantValue
@@ -77,8 +104,7 @@ namespace RestEase.SourceGenerator.Implementation
             ExplicitMethodDeclaration = new SymbolDisplayFormat(
                 globalNamespaceStyle: SymbolDisplayGlobalNamespaceStyle.Included,
                 typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces,
-                genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters
-                    | SymbolDisplayGenericsOptions.IncludeVariance,
+                genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters,
                 memberOptions: SymbolDisplayMemberOptions.IncludeParameters
                     | SymbolDisplayMemberOptions.IncludeConstantValue
                     | SymbolDisplayMemberOptions.IncludeRef,
